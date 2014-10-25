da8b9fb8ae1f9f168ee25b4c1b2443d4075ea0ce
LeahK <duello.1@osu.edu>
2014-10-25 12:17:11 -0700

    Update calc.js
    Usama's update plus additional update by me that includes additional methods

--- Project5/calc.js
+++ Project5/calc.js
@@ -1,74 +1,87 @@
-// String literals used for the operators
-ADD = "+";
-SUB = "-";
-MULT = "*";
-
-// The following holds the values needed for calculation. 
-// The first is the first operand, the next is the second operator, and the third is the operator. 
-operation = [0, 0, ""];
-
-// Used to control which operand is to be targeted
-operand = 0;
-
-// Wait for the document to load
-$(document).ready(function() {
-	
-	display = $("#display");			// Saves some redundancy
-	display.text(0);
-	
-	$(".digit").click(function() {
-		
-		// Update the current operand in the operation array
-		digit = parseInt($(this).text(), 10);
-		operation[operand] *= 10;
-		operation[operand] += digit;
-		
-		// Display the current operand
-		display.text(operation[operand]);
-	});
-	
-	$(".operator").click(function() {
-		
-		// Update the operator in the operation array
-		operator = $(this).text();
-		operation[2] = operator;
-		
-		// Change the current operand to the next
-		operand++;
-		operand = operand % 2;
-		
-		// Display the operator
-		display.text(operation[2]);
-	});
-	
-	$("#enter").click(function() {
-		
-		// Evaluate the operation
-		answer = 0;
-		
-		switch(operation[2]) {
-			
-			case ADD:	
-				result = operation[0] + operation[1];
-				break;
-			
-			case SUB:
-				result = operation[0] - operation[1];
-				break;
-			
-			case MULT:
-				result = operation[0] * operation[1];
-				break;
-			
-			default:
-				result = 0;
+var mem = 0; // stored value
+var numberStr_A="0";
+var numberStr_B ="";
+decimalPoint=true;
+function addDigit(digit) {
+	if(numberStr_A.length<31)
+	{
+		if(numberStr_A=="0")
+		{
+		numberStr_A=digit;
 		}
-		
-		// Display the result
-		display.text(result);
-		
-		// Reset the values
-		operation = [0, 0, ""];
-		operand = 0;
-	});
-}); 
+		else
+		{
+		numberStr_A=numberStr_A.concat("",digit);
+		}
+		display();
+	}
+}
+function addOperator(opToAdd){
+	if (isOperator() == -1)
+    {
+      numberStr_A=numberStr_A.concat(opToAdd);
+    }
+    display();
+}
+function squareRoot(){
+    // if it there is not an operator before
+    if (isOperator() == -1)
+    {
+      numberStr_B = Math.pow(eval(numberStr_A), 0.5);
+      display();
+    }
+}
+function addPoint(){
+	if(decimalPoint)
+	{
+	numberStr_A=numberStr_A.concat("",".");
+	decimalPoint=!decimalPoint;
+	display();
+	}
+}
+function clearDisplay(){
+	numberStr_A="0";
+	numberStr_B="";
+	decimalPoint=true;
+	display();
+}
+function memPlus()
+{
+    mem += numberStr_B;
+}
+function memStore()
+{
+    mem = numberStr_B;
+}
+function clearMem()
+{
+    mem = 0;
+}
+function memRecall()
+{
+    clearDisplay();
+    numberStr_A = ""; // get rid of the inital 0
+    numberStr_A += mem;
+    display();
+}
+  // function returns 0 if it is an operator and returns -1 if it is not
+function isOperator(){
+  	var isOp = 0;
+    var lengthStr = numberStr_A.length;
+    // if the char before is an operator, return -1
+    if (numberStr_A.charAt(lengthStr-1) != "+" && numberStr_A.charAt(lengthStr-1) != "-" && numberStr_A.charAt(lengthStr-1) != "/" && numberStr_A.charAt(lengthStr-1) != "*" )
+    {
+      isOp = -1;
+    }
+    return isOp;
+}
+function display()
+{
+	document.getElementById("display_A").innerHTML = numberStr_A;
+	document.getElementById("display_B").innerHTML = numberStr_B;
+}
+function evalStr()
+{
+	numberStr_B = eval(numberStr_A);
+    display();
+}
